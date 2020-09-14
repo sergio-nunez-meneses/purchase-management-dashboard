a8aa6179c9e8a8537d83843cc5746c1e730eb765
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2020-09-14 11:32:12 +0200

    corrected column name in products table

--- classes/models/IndexModel.php
+++ classes/models/IndexModel.php
@@ -5,16 +5,15 @@ class IndexModel extends Database
 
   public function get_all_products($id)
   {
-    $stmt = $this->run_query('SELECT * FROM products WHERE users_id = :id ORDER BY id DESC', ['id' => $id]);
+    $stmt = $this->run_query('SELECT * FROM products WHERE user_id = :id ORDER BY id DESC', ['id' => $id]);
     $products = $stmt->fetchAll();
     return $products;
   }
 
   public function get_last_products($id)
   {
-    $stmt = $this->run_query('SELECT * FROM products WHERE users_id = :id ORDER BY id DESC LIMIT 3', ['id' => $id]);
+    $stmt = $this->run_query('SELECT * FROM products WHERE user_id = :id ORDER BY id DESC LIMIT 3', ['id' => $id]);
     $product = $stmt->fetchAll();
     return $product;
   }
-
 }
