abf4be580cd5a84a3101c0e2787355d66f953870
Philpof <philippe.perechodov@free.fr>
2021-01-21 13:38:22 +0100

    root

--- tools/sql.php
+++ tools/sql.php
@@ -3,5 +3,5 @@ define('DB_NAME', 'purchase_management_dashboard');
 define('DB_HOST', 'localhost');
 define('DB_CHAR', 'utf8');
 define('DB_USER', 'root');
-define('DB_PWD', '');
+define('DB_PWD', 'root');
 define('PDO_OPTIONS', [ \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC, \PDO::ATTR_EMULATE_PREPARES => false, ]);
